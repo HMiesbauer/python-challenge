c5cc5422dbcb613839f0969e920e482504447e32
Holly Miesbauer <miesbah@gmail.com>
2024-01-15 16:17:13 -0800

    Corrected the errors for PyPoll and resubmitted assignment

--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -26,11 +26,15 @@ with open(election_data_csv) as csv_file:
    
     for row in election_results:
         total_votes_cast +=1
-
-#get list of candidates who got votes 
+       
+#get list of candidates who got votes (ChatGPT, personal communication, January 15, 2024)
+with open(election_data_csv) as csv_file:
+    election_results = csv.reader(csv_file)
+    header = next(election_results)       
+    candidate_list = set(row[2] for row in election_results)
 
             
-# get candidate names and count votes using a loop so can use vote counts for variables in print statement(for loop code from chatgpt, my variables)
+# get candidate names and count votes using a loop so can use vote counts for variables in print statement.(for loop code used (ChatGPT, personal communication, January 1, 2024) to debug + suggest improvements, my variables)
 with open(election_data_csv) as csv_file:
     election_results = csv.reader(csv_file)
     header = next(election_results)
@@ -49,11 +53,10 @@ with open(election_data_csv) as csv_file:
     deGette_percentage = (deGette_votes / total_votes_cast) * 100
     doane_percentage = (doane_votes / total_votes_cast) * 100
 
-#Who received the most votes(winner)  
-candidate_list_votes = [(stockham_votes), (deGette_votes), (doane_votes)] # list to get the candidate votes but need to pair with names
-most_votes = candidate_list_votes.index(max(candidate_list_votes))
-winner = candidate_list_votes[most_votes] 
-print(winner)
+#Who received the most votes(winner) - (ChatGPT, personal communication, January 15, 2024)
+
+candidate_list_votes = [("Charles Casper Stockham", stockham_votes), ("Diana DeGette", deGette_votes), ("Raymon Anthony Doane", doane_votes)]
+winner = max(candidate_list_votes, key=lambda x: x[1])
 
 #print output f statement from PyBank created with tutor David Chao
 output = f"""
@@ -65,9 +68,10 @@ Charles Casper Stockham: {stockham_percentage:.2f}% ({stockham_votes})
 Diana DeGette: {deGette_percentage:.2f}% ({deGette_votes})
 Raymon Anthony Doane: {doane_percentage:.2f}% ({doane_votes})
 -------------------------
-Winner: Diana DeGette
+Winner: {winner[0]}
 -------------------------
 """
+print(output)
 
 #save output to file
 with open("Analysis/election_results.txt", "w") as outfile:
